e201e12cbfb62ba8d187b4f753ad2d4ff853c1eb
Shad Azam <azamshad08@gmail.com>
2023-06-11 20:04:37 +0530

    update

--- src/components/SuggestioinVideoCard.jsx
+++ src/components/SuggestioinVideoCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SuggestioinVideoCard = () => {
   return (
     <div>
-      
+      SuggestioinVideoCard
     </div>
   )
 }
--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiOutlineLike } from "react-icons/ai";
+import { abbreviateNumber } from "js-abbreviation-number";
+
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
+import SuggestioinVideoCard from "./SuggestioinVideoCard";
 
 const VideoDetails = () => {
-  return (
-    <div>
-      videoDetails
-    </div>
-  )
-}
+  const [video, setVideo] = useState();
+  const [relatedVideos, setRelatedVideos] = useState();
+  const { id } = useParams();
+  const { setLoading } = useContext(Context);
+  useEffect(() => {}, [id]);
+  const fetchVideoDetails = () => {
+    setLoading(true);
+    fetchDataFromApi(`video/details/?id=${id}`);
+  };
 
-export default VideoDetails
+  return <div>videoDetails</div>;
+};
+
+export default VideoDetails;
